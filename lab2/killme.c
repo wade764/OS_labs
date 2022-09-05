cbcaba5b2016ea63370c12dcc40e5299c9c6fee2
Wade <wade@pop-os.localdomain>
2022-09-04 20:29:39 -0400

    Code is working for killme.c but running into an error for the numbers related to pid

--- lab2/killme.c
+++ lab2/killme.c
@@ -9,19 +9,40 @@
 // I have made a reference to a source that covered printing out FILE contents
 
 #define FN_LEN 256
+
 struct Options {
     bool using_h; // -h, human readable
-                  //bool using_a; // -a, print all
-                  //bool using_d; // -d, list dirs only
     bool using_f; // -f, has a file optarg
+
+    bool using_0; // added this line to account for a proccess id
+    bool using_1; // added this line to account for a proccess id
+    bool using_2; // added this line to account for a proccess id
+    bool using_3; // added this line to account for a proccess id
+    bool using_4; // added this line to account for a proccess id
+    bool using_5; // added this line to account for a proccess id
+    bool using_6; // added this line to account for a proccess id
+    bool using_7; // added this line to account for a proccess id
+    bool using_8; // added this line to account for a proccess id
+    bool using_9; // added this line to account for a proccess id
+
     char filename[FN_LEN]; // -f optarg
 };
 
 static void init_opts(struct Options* opts) {
     opts->using_h = false;
-    //opts->using_a = false;
-    //opts->using_d = false;
     opts->using_f = false;
+
+    opts->using_0 = false;
+    opts->using_1 = false;
+    opts->using_2 = false;
+    opts->using_3 = false;
+    opts->using_4 = false;
+    opts->using_5 = false;
+    opts->using_6 = false;
+    opts->using_7 = false;
+    opts->using_8 = false;
+    opts->using_9 = false;
+
     for (int i = 0; i < FN_LEN; i++)
         opts->filename[i] = 0;
 }
@@ -33,15 +54,30 @@ struct Options get_opts(int count, char* args[]) {
     init_opts(&opts);
     int opt;
 
-    while ((opt = getopt(count, args, ":f:had")) != -1) {
+    //*** 04Sep the issue at the moment is that you must use the - then the integer and not just 1234
+    // rethinking this approach but at the moment the current code works with input -h -f
+    // if you type -0123456789 it will print the number that is pressed but otherwise I am getting additional new lines printed coming from main function
+
+    while ((opt = getopt(count, args, ":if:hf0123456789")) != -1) { // the optional string before was ":f:had"
         switch (opt) {
             case 'h': opts.using_h = true; break;
-                      //case 'a': opts.using_a = true; break;
-                      //case 'd': opts.using_d = true; break;
+
             case 'f': 
                       opts.using_f = true; 
                       strcpy(opts.filename, optarg);
                       break;
+
+            case '0': opts.using_0 = true; break;
+            case '1': opts.using_1 = true; break;
+            case '2': opts.using_2 = true; break;
+            case '3': opts.using_3 = true; break;
+            case '4': opts.using_4 = true; break;
+            case '5': opts.using_5 = true; break;
+            case '6': opts.using_6 = true; break;
+            case '7': opts.using_7 = true; break;
+            case '8': opts.using_8 = true; break;
+            case '9': opts.using_9 = true; break;
+
             case ':': 
                       printf("-f needs a value\n");
                       break;
@@ -62,8 +98,18 @@ int main(int argc, char *argv[]) {
 
     struct Options o = get_opts(argc, argv);
     printf("%s\n", opts.using_h ? "Hello World" : "" /* print an empty string */ );
-    //printf("-a: %s\n", opts.using_a ? "true" : "false");
-    //printf("-d: %s\n", opts.using_d ? "true" : "false");
+    
+    printf("%c\n", opts.using_0 ? '0' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_1 ? '1' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_2 ? '2' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_3 ? '3' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_4 ? '4' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_5 ? '5' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_6 ? '6' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_7 ? '7' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_8 ? '8' : ' ' /* print a space char */ );
+    printf("%c\n", opts.using_9 ? '9' : ' ' /* print a space char */ );
+    
     if (opts.using_f) {
         //printf("-f: %s\n", opts.filename);
 
