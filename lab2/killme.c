60c114ffac43c672932ee2bb9772ae7bac8441ea
Wade <wade@pop-os.localdomain>
2022-09-05 19:55:24 -0400

    Stopping at the moment, I need to rethink my approach to all of this

--- lab2/killme.c
+++ lab2/killme.c
@@ -27,7 +27,7 @@ struct Options {
     //bool using_9; // added this line to account for a proccess id
 
     char filename[FN_LEN]; // -f optarg
-    
+
     // *** VERY IMPORTANT, if you declare a "flexible string i.e. []" you must put it at the end of the struct object!
     char process_id[128]; // This is a string used for the pid I am giving is a random size of 128
 };
@@ -35,7 +35,7 @@ struct Options {
 static void init_opts(struct Options* opts) {
     opts->using_h = false;
     opts->using_f = false;
-    
+
     //char empty_string[] = "\0";
 
     //opts->using_1 = false;
@@ -50,7 +50,7 @@ static void init_opts(struct Options* opts) {
 
     for (int i = 0; i < FN_LEN; i++)
         opts->filename[i] = 0;
-    
+
     // setting the process_id string to null just like the filename above
     for (int i = 0; i < 128; i++)
         opts->process_id[i] = 0;
@@ -73,7 +73,7 @@ struct Options get_opts(int count, char* args[]) {
     // if you type -0123456789 it will print the number that is pressed but otherwise I am getting additional new lines printed coming from main function
 
     while ((opt = getopt(count, args, ":f:had")) != -1) { // the optional string before was ":f:had"
-        
+
         switch (opt) {
             case 'h': opts.using_h = true; break;
 
@@ -82,68 +82,81 @@ struct Options get_opts(int count, char* args[]) {
                       strcpy(opts.filename, optarg);
                       break;
 
-            //case atoi(args): opts.using_0 = true; break;
-            //case "1": opts.using_1 = true; break;
-            //case "2": opts.using_2 = true; break;
-            //case "3": opts.using_3 = true; break;
-            //case "4": opts.using_4 = true; break;
-            //case "5": opts.using_5 = true; break;
-            //case "6": opts.using_6 = true; break;
-            //case "7": opts.using_7 = true; break;
-            //case "8": opts.using_8 = true; break;
-            //case "9": opts.using_9 = true; break;
+                      //case atoi(args): opts.using_0 = true; break;
+                      //case "1": opts.using_1 = true; break;
+                      //case "2": opts.using_2 = true; break;
+                      //case "3": opts.using_3 = true; break;
+                      //case "4": opts.using_4 = true; break;
+                      //case "5": opts.using_5 = true; break;
+                      //case "6": opts.using_6 = true; break;
+                      //case "7": opts.using_7 = true; break;
+                      //case "8": opts.using_8 = true; break;
+                      //case "9": opts.using_9 = true; break;
 
             case ':': 
                       printf("-f needs a value\n");
                       break;
             case '?': 
                       printf("Unknown option\n");
-                      
+
                       // **** DO NOT DELETE, uncomment after testing ****
                       //exit(-1);
         }
     }
-        //TEST
-        //printf("I am outside the for loop\n");
-        
-        // copied and modified from mainargs.c
-        for(int c = 0 ; count_temp>1 && &args_temp[1][0] != NULL; count_temp--,args_temp++, c++) { // changed from == '-' to != NULL
+    //TEST
+    //printf("I am outside the for loop\n");
+
+    // copied and modified from mainargs.c
+
+    //int c = 0;
+    //while(&args_temp[1][c] != NULL) {
+
+        for(int d = 0 ; count_temp>1 && &args_temp[1][0] != NULL; count_temp--,args_temp++, d++) { // changed from == '-' to != NULL
         // my thinking is that the numbers are not led by a - so as long as the string is not NULL it should enter this loop
 
-        //TEST
-        //printf("I am in the for loop\n");
-        //printf("this is argc: %d this is args_temp[1][1]: %d\n",count_temp, atoi(&args_temp[1][0]));
-            
-            switch(args_temp[1][0]) { // this should be args_temp[1][0]
-            case '1': // this must be a char because the args_temp[1][0] holds the ASCII value if the integer not the int itself
-                //opts.using_1 = malloc(16*sizeof(char));
+            //TEST
+            //printf("I am in the for loop\n");
+            //printf("this is argc: %d this is args_temp[1][0]: %d\n",count_temp, atoi(&args_temp[1][0]));
 
-                //TEST
-                //printf("inside switch\n");
-                
-                //char* one = "1";
-                //opts.process_id = "1";
-                strcat(opts.process_id, "1");
-                break;
-            //case 'u':
-            //    printf("-u flag\n");
-            //    uflg++;
-            //    continue;
-            //case 'n':
-            //    printf("-n flag\n");
-            //    nflg++;
-            //    continue;
-            //    continue;
-            //case 's':
-            //    printf("-s flag\n");
-            //    sflg++;
-            //    continue;
-            //default:
-            //    printf("invalid flag\n");
-            }
-        //break;
-    }
-    
+            //strcat()
+            //strncat() appends to the end with max size
+
+            //switch(args_temp[1][0]) { // this should be args_temp[1][0]
+            //    case '0': strncat(opts.process_id, "0", 127); break;
+            //    case '1': // this must be a char because the args_temp[1][0] holds the ASCII value if the integer not the int itself
+            //              //opts.using_1 = malloc(16*sizeof(char));
+
+            //              //TEST
+            //              //printf("inside switch\n");
+
+            //              //char* one = "1";
+            //              //opts.process_id = "1";
+            //              strncat(opts.process_id, "1", 127);
+            //              break;
+            //    case '2': strncat(opts.process_id, "2", 127); break;
+            //    case '3': strncat(opts.process_id, "3", 127); break;
+            //    case '4': strncat(opts.process_id, "4", 127); break;
+            //    case '5': strncat(opts.process_id, "5", 127); break;
+            //    case '6': strncat(opts.process_id, "6", 127); break;
+            //    case '7': strncat(opts.process_id, "7", 127); break;
+            //    case '8': strncat(opts.process_id, "8", 127); break;
+            //    case '9': strncat(opts.process_id, "9", 127); break;
+            //}
+
+            args_temp[1][0] == '0' ? strncat(opts.process_id, "0", 127) : 0; 
+            args_temp[1][0] == '1' ? strncat(opts.process_id, "1", 127) : 0; 
+            args_temp[1][0] == '2' ? strncat(opts.process_id, "2", 127) : 0; 
+            args_temp[1][0] == '3' ? strncat(opts.process_id, "3", 127) : 0; 
+            args_temp[1][0] == '4' ? strncat(opts.process_id, "4", 127) : 0; 
+            args_temp[1][0] == '5' ? strncat(opts.process_id, "5", 127) : 0; 
+            args_temp[1][0] == '6' ? strncat(opts.process_id, "6", 127) : 0; 
+            args_temp[1][0] == '7' ? strncat(opts.process_id, "7", 127) : 0; 
+            args_temp[1][0] == '8' ? strncat(opts.process_id, "8", 127) : 0; 
+            args_temp[1][0] == '9' ? strncat(opts.process_id, "9", 127) : 0; 
+            //break;
+        }
+        //c++;
+    //}
 
     return opts;
 }
@@ -156,9 +169,9 @@ int main(int argc, char *argv[]) {
 
     struct Options o = get_opts(argc, argv);
     printf("%s", opts.using_h ? "Hello World\n" : "" /* print an empty string */ );
-    
+
     //printf("%s\n", (strcmp(opts.process_id, "1") == 0) ? opts.process_id : "test" /* print an empty string */ );
-    
+
     //printf("%s", opts.using_1 ? "1/n" : "" /* print an empty string */ );
     //printf("%s", opts.using_2 ? "2/n" : "" /* print an empty string */ );
     //printf("%s", opts.using_3 ? "3/n" : "" /* print an empty string */ );
@@ -168,7 +181,7 @@ int main(int argc, char *argv[]) {
     //printf("%s", opts.using_7 ? "7/n" : "" /* print an empty string */ );
     //printf("%s", opts.using_8 ? "8/n" : "" /* print an empty string */ );
     //printf("%s", opts.using_9 ? "9/n" : "" /* print an empty string */ );
-    
+
     if (opts.using_f) {
         //printf("-f: %s\n", opts.filename);
 
