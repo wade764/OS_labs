f43b1950446dcc135d1ae3e4d1457235eec97ff7
Wade <wade@pop-os.localdomain>
2022-09-05 17:17:44 -0400

    still working on killme I have it going now, also added a file testargs.c this is a copy of mainargs.c I was using for learning purposes

--- lab2/killme.c
+++ lab2/killme.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include<string.h> // added to use the strcat() function
+
 // A lot of this code was copied from maingetopts, I have modified parts as needed
 // I have made a reference to a source that covered printing out FILE contents
 
@@ -14,37 +16,45 @@ struct Options {
     bool using_h; // -h, human readable
     bool using_f; // -f, has a file optarg
 
-    bool using_0; // added this line to account for a proccess id
-    bool using_1; // added this line to account for a proccess id
-    bool using_2; // added this line to account for a proccess id
-    bool using_3; // added this line to account for a proccess id
-    bool using_4; // added this line to account for a proccess id
-    bool using_5; // added this line to account for a proccess id
-    bool using_6; // added this line to account for a proccess id
-    bool using_7; // added this line to account for a proccess id
-    bool using_8; // added this line to account for a proccess id
-    bool using_9; // added this line to account for a proccess id
+    //bool using_1; // added this line to account for a proccess id
+    //bool using_2; // added this line to account for a proccess id
+    //bool using_3; // added this line to account for a proccess id
+    //bool using_4; // added this line to account for a proccess id
+    //bool using_5; // added this line to account for a proccess id
+    //bool using_6; // added this line to account for a proccess id
+    //bool using_7; // added this line to account for a proccess id
+    //bool using_8; // added this line to account for a proccess id
+    //bool using_9; // added this line to account for a proccess id
 
     char filename[FN_LEN]; // -f optarg
+    
+    // *** VERY IMPORTANT, if you declare a "flexible string i.e. []" you must put it at the end of the struct object!
+    char process_id[128]; // This is a string used for the pid I am giving is a random size of 128
 };
 
 static void init_opts(struct Options* opts) {
     opts->using_h = false;
     opts->using_f = false;
+    
+    //char empty_string[] = "\0";
 
-    opts->using_0 = false;
-    opts->using_1 = false;
-    opts->using_2 = false;
-    opts->using_3 = false;
-    opts->using_4 = false;
-    opts->using_5 = false;
-    opts->using_6 = false;
-    opts->using_7 = false;
-    opts->using_8 = false;
-    opts->using_9 = false;
+    //opts->using_1 = false;
+    //opts->using_2 = false;
+    //opts->using_3 = false;
+    //opts->using_4 = false;
+    //opts->using_5 = false;
+    //opts->using_6 = false;
+    //opts->using_7 = false;
+    //opts->using_8 = false;
+    //opts->using_9 = false;
 
     for (int i = 0; i < FN_LEN; i++)
         opts->filename[i] = 0;
+    
+    // setting the process_id string to null just like the filename above
+    for (int i = 0; i < 128; i++)
+        opts->process_id[i] = 0;
+
 }
 
 struct Options opts;
@@ -52,13 +62,18 @@ struct Options opts;
 struct Options get_opts(int count, char* args[]) {
 
     init_opts(&opts);
-    int opt;
+    int opt; 
+
+    // variables used in the for loop below
+    int count_temp = count;
+    char ** args_temp = args;
 
     //*** 04Sep the issue at the moment is that you must use the - then the integer and not just 1234
     // rethinking this approach but at the moment the current code works with input -h -f
     // if you type -0123456789 it will print the number that is pressed but otherwise I am getting additional new lines printed coming from main function
 
-    while ((opt = getopt(count, args, ":if:hf0123456789")) != -1) { // the optional string before was ":f:had"
+    while ((opt = getopt(count, args, ":f:had")) != -1) { // the optional string before was ":f:had"
+        
         switch (opt) {
             case 'h': opts.using_h = true; break;
 
@@ -67,25 +82,68 @@ struct Options get_opts(int count, char* args[]) {
                       strcpy(opts.filename, optarg);
                       break;
 
-            case '0': opts.using_0 = true; break;
-            case '1': opts.using_1 = true; break;
-            case '2': opts.using_2 = true; break;
-            case '3': opts.using_3 = true; break;
-            case '4': opts.using_4 = true; break;
-            case '5': opts.using_5 = true; break;
-            case '6': opts.using_6 = true; break;
-            case '7': opts.using_7 = true; break;
-            case '8': opts.using_8 = true; break;
-            case '9': opts.using_9 = true; break;
+            //case atoi(args): opts.using_0 = true; break;
+            //case "1": opts.using_1 = true; break;
+            //case "2": opts.using_2 = true; break;
+            //case "3": opts.using_3 = true; break;
+            //case "4": opts.using_4 = true; break;
+            //case "5": opts.using_5 = true; break;
+            //case "6": opts.using_6 = true; break;
+            //case "7": opts.using_7 = true; break;
+            //case "8": opts.using_8 = true; break;
+            //case "9": opts.using_9 = true; break;
 
             case ':': 
                       printf("-f needs a value\n");
                       break;
             case '?': 
                       printf("Unknown option\n");
-                      exit(-1);
+                      
+                      // **** DO NOT DELETE, uncomment after testing ****
+                      //exit(-1);
         }
     }
+        //TEST
+        //printf("I am outside the for loop\n");
+        
+        // copied and modified from mainargs.c
+        for(int c = 0 ; count_temp>1 && &args_temp[1][0] != NULL; count_temp--,args_temp++, c++) { // changed from == '-' to != NULL
+        // my thinking is that the numbers are not led by a - so as long as the string is not NULL it should enter this loop
+
+        //TEST
+        //printf("I am in the for loop\n");
+        //printf("this is argc: %d this is args_temp[1][1]: %d\n",count_temp, atoi(&args_temp[1][0]));
+            
+            switch(args_temp[1][0]) { // this should be args_temp[1][0]
+            case '1': // this must be a char because the args_temp[1][0] holds the ASCII value if the integer not the int itself
+                //opts.using_1 = malloc(16*sizeof(char));
+
+                //TEST
+                //printf("inside switch\n");
+                
+                //char* one = "1";
+                //opts.process_id = "1";
+                strcat(opts.process_id, "1");
+                break;
+            //case 'u':
+            //    printf("-u flag\n");
+            //    uflg++;
+            //    continue;
+            //case 'n':
+            //    printf("-n flag\n");
+            //    nflg++;
+            //    continue;
+            //    continue;
+            //case 's':
+            //    printf("-s flag\n");
+            //    sflg++;
+            //    continue;
+            //default:
+            //    printf("invalid flag\n");
+            }
+        //break;
+    }
+    
 
     return opts;
 }
@@ -97,18 +155,19 @@ int main(int argc, char *argv[]) {
     FILE *fptr;
 
     struct Options o = get_opts(argc, argv);
-    printf("%s\n", opts.using_h ? "Hello World" : "" /* print an empty string */ );
+    printf("%s", opts.using_h ? "Hello World\n" : "" /* print an empty string */ );
     
-    printf("%c\n", opts.using_0 ? '0' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_1 ? '1' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_2 ? '2' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_3 ? '3' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_4 ? '4' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_5 ? '5' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_6 ? '6' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_7 ? '7' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_8 ? '8' : ' ' /* print a space char */ );
-    printf("%c\n", opts.using_9 ? '9' : ' ' /* print a space char */ );
+    //printf("%s\n", (strcmp(opts.process_id, "1") == 0) ? opts.process_id : "test" /* print an empty string */ );
+    
+    //printf("%s", opts.using_1 ? "1/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_2 ? "2/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_3 ? "3/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_4 ? "4/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_5 ? "5/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_6 ? "6/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_7 ? "7/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_8 ? "8/n" : "" /* print an empty string */ );
+    //printf("%s", opts.using_9 ? "9/n" : "" /* print an empty string */ );
     
     if (opts.using_f) {
         //printf("-f: %s\n", opts.filename);
@@ -133,6 +192,13 @@ int main(int argc, char *argv[]) {
 
         fclose(fptr);
         // **
+    }
+
+    if (strlen(opts.process_id) != 0) { // if the string is not empty
+        for (int i = 0; i < 128; i++) {
+            printf("%c",opts.process_id[i]);
+        }
+        printf("\n");
     }
 
     //for (int i = 0; i < 25; i++)
