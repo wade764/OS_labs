3c9cb5eb3cc43cf833a651a6003b477b4f0739a1
Wade <wade@pop-os.localdomain>
2022-09-14 19:52:07 -0400

    our_q9.c works but is also a little buggy, first two lines of output are test prints

--- lab3/our_q9.c
+++ lab3/our_q9.c
@@ -23,38 +23,109 @@ h. closes pipes
 int main(int argc, char **argv) {
     int p[2];
     char *excargv[3];
+    
+    //*** gdb says this causes a SIGPIPE, broken pipe
     pipe(p);
-    if(fork() == 0) {
-        printf("Left Child\n");
-        dup2(p[1], 1); // dup pipe write end on top of stdout
+
+    char aString[100];
+    if(argc == 2){
+        strcpy(aString, argv[1]);
+    } else { // not enough arguments
+        printf("Please enter a string: ./our_p9.c <string>\n");
+        scanf("%s",aString);
+    }
+
+    if(fork() == 0) { // The child process
+
+        //TEST
+        printf("I am the child\n");
+
+        dup2(p[0], 0);
+        char buf[100];
+        int r = read(0, buf, 80);
+
+        close(p[0]);
+        close(p[1]);
+
+        // now reopening the new pipe connecting back to parent
+        //pipe(p);
+
+        dup2(p[1], 1); // this time going to stdout
+
+        char *concatMe = {"HelloGusty"};
+        int concatLen = strlen(concatMe), total = concatLen + r; // adding r because its the len of buf the original string
+        strcat(buf, concatMe);
+
+        buf[total] = 0; // setting end of the buf array to NULL
+        
+        write(1, buf, total);
+
         close(p[0]);   // close pipe fd's
         close(p[1]);
-        excargv[0] = "ls";
-        excargv[1] = 0;
-        execvp(excargv[0], excargv);
-        printf("HERE\n");
-    } 
-    else {
-        if (fork() == 0) {
-            printf("Right Child\n");
-            dup2(p[0], 0); // dup pipe read end on top of stdin
-            close(p[0]);   // close pipe fd's
-            close(p[1]);
-            excargv[0] = "grep";
-            if (argc == 2)
-                excargv[1] = argv[1];
-            else
-                excargv[1] = "p1";
-            excargv[2] = 0;
-            execvp(excargv[0], excargv);
-        }
-        else {
-            printf("Parent\n");
+        
+        //excargv[0] = "ls";
+        //excargv[1] = 0;
+        //execvp(excargv[0], excargv);
+        //printf("HERE\n");
+    } else {
+        //if (fork() == 0) {
+            //printf("Right Child\n");
+            //dup2(p[0], 0); // dup pipe read end on top of stdin
+            //close(p[0]);   // close pipe fd's
+            //close(p[1]);
+            //excargv[0] = "grep";
+            //if (argc == 2)
+            //    excargv[1] = argv[1];
+            //else
+            //    excargv[1] = "p1";
+            //excargv[2] = 0;
+            //execvp(excargv[0], excargv);
+        //}
+        //else { // The parent
+
+            //TEST
+            printf("I am the parent\n");
+
+            dup2(p[1], 1); // opening the write side of pipe to stdout
+
+            write(1, aString, strlen(aString));
+            //printf("Parent\n");
+            
             close(p[0]);
             close(p[1]);
-            wait(NULL);
-            wait(NULL);
+
+            wait(NULL); // waiting on the child to terminate
+
+            // now the child process is done reopening the read end of the second pipe
+
+            dup2(p[0], 0); // stdin
+
+            char buf[100];
+            int r = read(0, buf, 80);
+            buf[r] = 0;
+            
+            close(p[0]);
+            close(p[1]);
+            
+            int i = 0;
+            
+            printf("%s\n", buf);
+            
+            //while(buf[i] != 0) {
+            //    printf("%c",buf[i]);
+            //    i++;
+
+            //    //TEST
+            //    printf("Stuck in loop?\n");
+            //}
+            //printf("\n");
+
+            //The program acts like it is stuck in a loop I am adding the line below to exit
+            //exit(0);
+
+            // The exit() does not work I tried commenting out wait that does not work either
+
         }
-    }
+    //}
     return 0;
 }
